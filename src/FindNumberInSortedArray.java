ed9dab615c5c81234400ac484c665f4a325366a7
Chris Bls <chrisb2603@gmail.com>
2022-07-30 19:46:58 +0300

    First Commit

---
+++ src/FindNumberInSortedArray.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+public class FindNumberInSortedArray {
+
+    public static List<Integer> convertIntegerSequenceToList(String input) {
+        List<Integer> list = new ArrayList<>();
+        String[] elements = input.split("\\s+");
+        for (String element : elements) {
+            list.add(Integer.valueOf(element));
+        }
+        return list;
+    }
+
+    public static List<Integer> convertFileSequenceToList(File input) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(input));
+        String line = br.readLine();
+        br.close();
+        return convertIntegerSequenceToList(line);
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        File sm = new File("1-sm.txt");
+        System.out.print("Inserted File: "+convertFileSequenceToList(sm));
+        List<Integer> A = convertFileSequenceToList(sm);
+
+        int x = 8;
+        int count = 0;
+        int j = A.size()/2;
+        int mid = A.get(j);
+
+        if (x > mid) {
+            for(int i = A.size()/2 + 1; i < A.size(); i++) {
+                if(A.get(i).equals(x)) {
+                    count = count + 1;
+                }
+            }
+        }
+        else if(x == mid) {
+            count = count +1;
+            int n = 1;
+            while(A.get(j-n) == x) {
+                count = count + 1;
+                n = n + 1;
+            }
+            n = 1;
+            while(A.get(j+n) == x) {
+                count = count + 1;
+                n = n + 1;
+            }
+        }
+
+        else {
+            for(int i = 0; i<A.size()/2;i++) {
+                if(A.get(i).equals(x)) {
+                    count = count + 1;
+                }
+            }
+        }
+        System.out.print("\nX = "+x+" appears "+count+" times.");
+    }
+}
---
+++ src/ReadMe.txt
@@ -0,0 +1,12 @@
+This program takes as an input a sorted Array and counts how many times a given number x (8 in this state)
+appears inside the array, using O(logn) complexity
+
+TODO:
+	- The file containing the array needs to be named "1-sm.txt" in .txt form
+	- The file must contain only numbers sorted and distinguished through space (e.g. 1 2 3 4 5 6 7 8 9 10 11 12 13)
+
+Running through:
+- cmd (move "1-sm.txt" file to same folder with class)
+- Intellij
+
+Java Version: jdk-11.0.11
